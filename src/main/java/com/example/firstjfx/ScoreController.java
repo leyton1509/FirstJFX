c08b10c835f814464b415996cdd22901b55e646d
leyton1509 <taylor92@lancaster.ac.uk>
2022-12-01 10:12:08 +0000

    Added javadoc to the score controller

--- src/main/java/com/example/firstjfx/ScoreController.java
+++ src/main/java/com/example/firstjfx/ScoreController.java
@@ -11,29 +11,51 @@ import java.util.ResourceBundle;
 
 public class ScoreController implements Initializable {
 
+    /**
+     * The questions text area
+     */
     @FXML
     protected TextArea questionTextArea;
 
+    /**
+     * The iser input text area
+     */
     @FXML
     protected TextArea userTextArea;
 
+    /**
+     * The answer text area
+     */
     @FXML
     protected TextArea answerTextArea;
 
+    /**
+     * The score label
+     */
     @FXML
     protected Label scoreLabel;
 
+    /**
+     * The question answers singleton class
+     */
     private QuestionAnswers qaClass;
 
 
-
+    /**
+     * Called when the scene is created
+     * Sets the questions, answers and user inputs to the text boxes
+     * and tells the user the score they got
+     * @param url The url
+     * @param resourceBundle The resources
+     */
     @Override @FXML
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Gets the instance of question answers, a singleton means theres only ever one instance
         qaClass = QuestionAnswers.getInstance();
         QuestionHandler qh = qaClass.getQuestionHandler();
         ArrayList<String> ans = qaClass.getAnswers();
 
-
+        // Builds the strings from the variables
         StringBuilder questionText = new StringBuilder();
         StringBuilder answerText = new StringBuilder();
         StringBuilder userAnswerText = new StringBuilder();
